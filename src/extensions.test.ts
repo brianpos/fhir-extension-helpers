6326c10678c96b10b1b2fd8c95b18c4e88b66eca
Brian Postlethwaite <brian.postlethwaite@health.telstra.com>
2022-01-25 12:59:19 +1100

    An example testing out the usage with primitive extension types.

--- src/extensions.test.ts
+++ src/extensions.test.ts
@@ -1,7 +1,7 @@
 import exHelpers from './extensions';
 
 test('string get/set/clear extension value', () => {
-    let sample:fhir4.Coding = { system: 'system', code: 'c', display: 'blah'};
+    let sample: fhir4.Coding = { system: 'system', code: 'c', display: 'blah' };
     expect(JSON.stringify(sample)).toBe('{"system":"system","code":"c","display":"blah"}');
     exHelpers.setExtension(sample, { url: 'exturl', valueString: 'test' });
     let result = exHelpers.getExtensionStringValue(sample, 'exturl');
@@ -40,7 +40,7 @@ test('boolean get/set extension value', () => {
 
 // Test multiple values
 test('batch get/set/clear extension value', () => {
-    let sample:fhir4.Coding = { system: 'system', code: 'c', display: 'blah'};
+    let sample: fhir4.Coding = { system: 'system', code: 'c', display: 'blah' };
     expect(JSON.stringify(sample)).toBe('{"system":"system","code":"c","display":"blah"}');
     exHelpers.setExtension(sample, { url: 'exturl', valueString: 'test' });
     exHelpers.addExtension(sample, { url: 'exturl', valueString: 'test2' });
@@ -49,7 +49,7 @@ test('batch get/set/clear extension value', () => {
     expect(result).toBe('test');
 
     let results = exHelpers.getExtensionStringValues(sample, 'exturl');
-    expect(results).toStrictEqual(['test','test2']);
+    expect(results).toStrictEqual(['test', 'test2']);
 
     exHelpers.setExtension(sample, { url: 'exturl', valueString: 'test3' });
     result = exHelpers.getExtensionStringValue(sample, 'exturl');
@@ -60,5 +60,23 @@ test('batch get/set/clear extension value', () => {
     expect(JSON.stringify(sample)).toBe('{"system":"system","code":"c","display":"blah"}');
     result = exHelpers.getExtensionStringValue(sample, 'exturl');
     expect(result).toBe(undefined);
+})
+
+
+// Test setting primitive property extension
+test('batch get/set/clear extension value', () => {
+    let patient: fhir4.Patient = { resourceType: 'Patient', birthDate: '1970-01-01' };
+    expect(JSON.stringify(patient)).toBe('{"resourceType":"Patient","birthDate":"1970-01-01"}');
+    exHelpers.setExtension(patient, { url: 'exturl', valueString: 'test' });
+    expect(JSON.stringify(patient)).toBe('{"resourceType":"Patient","birthDate":"1970-01-01","extension":[{"url":"exturl","valueString":"test"}]}');
+
+    let result = exHelpers.getExtensionStringValue(patient, 'exturl');
+    expect(result).toBe('test');
+    exHelpers.clearExtension(patient, 'exturl');
+
+    // for native property extensions need to create the extensions too
+    patient._birthDate = { extension: [] };
+    exHelpers.setExtension(patient._birthDate, { url: 'http://example.org/time', valueString: 'test2' });
+    expect(JSON.stringify(patient)).toBe('{"resourceType":"Patient","birthDate":"1970-01-01","_birthDate":{"extension":[{"url":"http://example.org/time","valueString":"test2"}]}}');
 })
 
